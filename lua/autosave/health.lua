b19b4a34733b15ece38ba5db6a0b180b06e123a9
brianhuster <phambinhanctb2004@gmail.com>
2024-11-07 11:50:32 +0700

    update

--- lua/autosave/health.lua
+++ lua/autosave/health.lua
@@ -11,9 +11,9 @@ M.check = function()
 	health.start('Check requirements')
 	local vimver = string.format('%s.%s.%s', vim.version().major, vim.version().minor, vim.version().patch)
 	if vim.fn.has('nvim-0.9') == 0 then
-		health.warn('Neovim version is too old', 'Update to Neovim 0.9 or later')
+		health.error('Neovim version is too old', 'Update to Neovim 0.9 or later')
 	else
-		health.ok('Neovim version is up to date', vimver)
+		health.ok('Neovim %s is compatible with with autosave.nvim', vimver)
 	end
 end
 
